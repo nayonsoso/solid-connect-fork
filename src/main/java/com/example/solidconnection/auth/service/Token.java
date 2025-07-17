81cd5a40645424d5d8e220b832b01b29ebe3c0a7
nayonsoso <panda0329@naver.com>
2025-07-17 19:34:34 +0900

    토큰 객체 생성할 때 빠뜨림!

--- src/main/java/com/example/solidconnection/auth/service/Token.java
+++ src/main/java/com/example/solidconnection/auth/service/Token.java
@@ -1,7 +1,9 @@
 package com.example.solidconnection.auth.service;
 
 import com.example.solidconnection.auth.domain.TokenType;
+import lombok.Getter;
 
+@Getter
 public class Token {
 
     protected final Subject subject;
